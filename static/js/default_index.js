ec174d2bfc46830a36e5e735870871ca8ef86ee7
Chris Gradwohl <christophergradwohl@gmail.com>
2016-11-29 16:35:16 -0800

    added event title and description functionality

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -1,48 +1,8 @@
 // This is the js for the default/index.html view.
 var app = function() {
-
     var self = {};
-
     Vue.config.silent = false; // show all warnings
 
-    // Extends an array
-    self.extend = function(a, b) {
-        for (var i = 0; i < b.length; i++) {
-            a.push(b[i]);
-        }
-    };
-
-    self.load_events = function() {
-        $.get(
-            getMarkerUrl,
-            function(data) {
-                if (self.vue.events != data.events){
-                    self.vue.map.removeMarkers();
-                    self.vue.events = data.events;
-                    self.show_events();
-                }
-            }
-        )
-    };
-
-    self.show_events = function() {
-        self.vue.map.addMarkers(self.vue.events.map(make_marker_dict));
-    };
-
-    self.auto_refresh = function () {
-        setInterval(
-            self.load_events, 10000
-        )
-    };
-
-    self.initmap = function() {
-        self.vue.map = new GMaps({
-            el: '#map',
-            lat: 36.9914,
-            lng: -122.0609
-        });
-
-    };
 
     make_marker_dict = function(event) {
         return {
@@ -56,51 +16,83 @@ var app = function() {
         };
     };
 
+
     self.add_to_map = function(event) {
         self.vue.map.addMarker(make_marker_dict(event));
     };
 
-    self.addMarker = function(latt, long) {
+
+    self.add_event_marker = function(latt, long, title, desc) {
         var date = new Date();
-        var muhstring = date.toISOString();
-        console.log(map);
+        var date_string = date.toISOString();
+
         $.post(addEventUrl,
             {
-                latitude: latt,
-                longitude: long,
-                title: 'BOW BOW BOW BOW BOW BOW',
-                description: 'BAZINGA',
-                date: muhstring
+                latitude   : latt,
+                longitude  : long,
+                title      : '<p>'+title+'</p>',
+                description: '<p>'+desc+'</p>',
+                date       : date_string
             },
             function(data) {
                 self.add_to_map(data);
-                console.log("AWW YUS");
             }
-            )
+        )
     };
 
 
-    // Complete as needed.
+    self.initmap = function() {
+        self.vue.map = new GMaps({
+            el : '#map',
+            lat: 36.9914,
+            lng: -122.0609
+        });
+
+    };
+
+
+    self.load_events = function() {
+        $.get(getMarkerUrl, function(data) {
+            if (self.vue.events != data.events){
+                self.vue.map.removeMarkers();
+                self.vue.events = data.events;
+                self.show_events();
+            }
+        })
+    };
+
+
+    self.show_events = function() {
+        self.vue.map.addMarkers(self.vue.events.map(make_marker_dict));
+    };
+
+
+    self.auto_refresh = function () {
+        setInterval(self.load_events, 10000)
+    };
+
+
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            has_more: false,
-            page: 'event_view',
-            events: [],
-            markers: [],
-            latt: 0,
-            long: 0,
-            event_name: '',
-            map: null
+            has_more  : false,
+            page      : 'event_view',
+            events    : [],
+            markers   : [],
+            latt      : 36.99,
+            long      : -122.05,
+            title     : '',
+            desc      : '',
+            map       : null
         },
         methods: {
-            get_more: self.get_more,
-            initmap: self.initmap,
-            addMarker: self.addMarker,
-            map: self.map,
-            logMap: self.logMap
+            get_more        : self.get_more,
+            initmap         : self.initmap,
+            add_event_marker: self.add_event_marker,
+            map             : self.map,
+            logMap          : self.logMap
         }
 
     });
@@ -108,9 +100,6 @@ var app = function() {
     self.initmap(); // googleializes the map on reload
     self.load_events(); //first load
     self.auto_refresh(); //set to refresh page so we see all events
-    //self.addMarker();
-
-
     return self;
 };
 
